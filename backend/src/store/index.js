05b916499abe1ca3dd7d9e2798f258c3b7e68cff
Aradhya Jain <aradhyajain471@gmail.com>
2023-07-24 11:21:27 +0530

    First Commit, nothing is working rn

---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state: {
+        test: ' Sample text'
+    },
+    getters:{},
+    actions:{},
+    mutation:{},
+})
+
+export default store
